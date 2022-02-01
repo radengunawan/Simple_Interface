f62603cd8ae002e3fd317224835e7a63cf0164fc
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-02-01 10:30:21 +0800

    ALL method in interfaces MUST be implemented, even with empty implementation

--- Simple_Interface/Ikan.cs
+++ Simple_Interface/Ikan.cs
@@ -4,12 +4,27 @@ using System.Text;
 
 namespace Simple_Interface
 {
-    class Ikan 
+    class Ikan : IMemangsa
     {
         public void caraMakan()
         {
-            Console.WriteLine("digoreng, dibakar");
+            Console.WriteLine("Ikan bisa digoreng ato dibakar");
 
         }
-    }
+
+         public void Memakan()
+        {
+            Console.WriteLine("Ikan suka memakan alga, atau ikan2 kecil");
+        }
+
+        public void Memburu() {
+            Console.WriteLine("Ikan suka berburu alga, atau ikan2 kecil");
+        }
+
+        public void Menerkam()
+        {
+            Console.WriteLine("Ikan tidak bisa menerkam");
+        }
+
+    } //END class
 }
--- Simple_Interface/Program.cs
+++ Simple_Interface/Program.cs
@@ -8,7 +8,26 @@ namespace Simple_Interface
         {
            Ikan ikn = new Ikan();
 
+            //ikn.caraMakan();
+
+            //Elang eln = new Elang();
+
+            //Console.WriteLine("Eagle strategies:");
+            //eln.Memburu();
+            //eln.Menerkam();
+            //eln.Memakan();
+            //Kelinci klc = new Kelinci();
+
+            //Console.WriteLine("Rabbit strategies");
+            //klc.MendengarLawan();
+            //klc.MelihatLawan();
+            //klc.Menghindar();
+            //klc.MengecohLawan();
+            //klc.MemilihJalan();
+            ikn.Memakan();
             ikn.caraMakan();
+            ikn.Menerkam();
+            ikn.Memburu();
 
         }
     }
