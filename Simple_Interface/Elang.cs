89c4e6bced5fd4d75f5f0c63e9991e2be3cb98fb
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-02-01 10:01:00 +0800

    elanga implement Imemangsa

--- Simple_Interface/Elang.cs
+++ Simple_Interface/Elang.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Simple_Interface
 {
-    class Elang
+    class Elang : IMemangsa
     {
+        public void Memburu()
+        {
+            Console.WriteLine("Elang suka berburu di hutan atau perairan");
+
+        }
+        public void Menerkam()
+        {
+            Console.WriteLine("Elang suka nerkam lawan pake kaki-nya");
+        }
+        public void Memakan()
+        {
+            Console.WriteLine("Elang memakan mangsa setelah mangsa dipastikan mati");
+        }
     }
 }
